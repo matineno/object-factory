db915909c7700615b1cd3d2217cc6c26a1410638
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-03-22 16:50:01 -0500

    worked on the project but NOT COMPLETED

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,40 +1,127 @@
-'use strict';
+// 'use strict';
 
-//import movies from './movies.js';
+// // Basic utility function
+// function onEvent(event, selector, callback) {
+//   return selector.addEventListener(event, callback);
+// }
+
+// function select(selector, parent = document) {
+//   return parent.querySelector(selector);
+// }
+
+// const shapeButton = select('#shape');
+// const colorButton = select('#color');
+// const createButton = select('#create-button');
+// const grid = select('#grid');
+
+// let selectShape = '';
+// let selectColor = '';
+
+// onEvent('click', shapeButton, () => {
+//   selectShape = shapeButton.value;
+// });
+
+// onEvent('click', colorButton, () => {
+//   selectColor = colorButton.value;
+// });
+
+// class Shape {
+//   constructor(name, color) {
+//     this._name = name;
+//     this._color = color;
+//   }
+
+//   getName() {
+//     return this._name;
+//   }
+
+//   getColor() {
+//     return this._color;
+//   }
+
+//   getInfo() {
+//     return `Name: ${this._name}, Color: ${this._color}`;
+//   }
+// }
+
+// function createShape() {
+//   const newShape = new Shape(selectShape, selectColor);
+
+//   const shapeDiv = document.createElement('div');
+//   shapeDiv.classList.add('shape');
+//   shapeDiv.classList.add(newShape._name);
+//   shapeDiv.classList.add(newShape._color);
+//   //shapeDiv.textContent = `${newShape.getName()} (${newShape.getColor()})`;
+
+//   shapeDiv.addEventListener('click', () => {
+//     alert(newShape.getInfo());
+//   });
+
+//   grid.appendChild(shapeDiv);
+// }
+
+// onEvent('click', createButton, createShape);
+
+'use strict';
 
 // Basic utility function
 function onEvent(event, selector, callback) {
   return selector.addEventListener(event, callback);
 }
 
-function getElement(selector, parent = document) {
-  return parent.getElementById(selector);
-}
-
 function select(selector, parent = document) {
   return parent.querySelector(selector);
 }
 
-function selectAll(selector, parent = document) {
-  return [...parent.querySelectorAll(selector)];
-}
+const shapeSelect = select('#shape');
+const colorSelect = select('#color'); 
+const createButton = select('#create-button');
+const grid = select('#grid');
 
-function print(arg) {
-  console.log(arg);
-}
+let selectShape = '';
+let selectColor = '';
 
-const searchBar = select('.search-bar');
-const searchButton = select('.search-button');
-const dropList = select('.drop-list');
-
-onEvent('click', searchBar, function() {
-    dropList.classList.toggle('visible');
+onEvent('change', shapeSelect, () => { 
+  selectShape = shapeSelect.value; 
 });
-  
 
+onEvent('change', colorSelect, () => { 
+  selectColor = colorSelect.value;
+});
 
-// document.addEventListener('keydown', function(input) {
-//     if (input.key > 2) {
+class Shape {
+  constructor(name, color) {
+    this._name = name;
+    this._color = color;
+  }
 
-//     }
-// });
+  getName() {
+    return this._name;
+  }
+
+  getColor() {
+    return this._color;
+  }
+
+  getInfo() {
+    return `Name: ${this._name}, Color: ${this._color}`;
+  }
+}
+
+function createShape() {
+  const newShape = new Shape(selectShape, selectColor);
+
+  const shapeDiv = document.createElement('div');
+  shapeDiv.classList.add('shape');
+  shapeDiv.classList.add(newShape._name);
+  shapeDiv.classList.add(newShape._color);
+  //shapeDiv.textContent = `${newShape.getName()} (${newShape.getColor()})`;
+
+  shapeDiv.addEventListener('click', () => {
+    alert(newShape.getInfo());
+  });
+
+  grid.appendChild(shapeDiv);
+}
+
+onEvent('click', createButton, createShape);
